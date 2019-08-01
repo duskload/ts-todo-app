3f9d48fe108f50556748ba43b5f932df47140a34
Michael Laktionov <duskload@gmail.com>
2019-08-01 08:27:36 +0300

    add material-ui. add simple todo app skeleton

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,12 +1,39 @@
 import React from 'react'
+import TodoList from '../../components/TodoList'
+
 import './App.css'
 
-const App: React.FC = () => {
-  return (
-    <div className="app">
-      <h1>ToDo App</h1>
-    </div>
-  )
+export interface ITodo {
+  text: string
+}
+
+interface IAppState {
+  todos: Array<ITodo>
+}
+
+class App extends React.Component<{}, IAppState> {
+  state: IAppState = {
+    todos: []
+  }
+
+  addItem = (text: string) => {
+    const item = { text }
+
+    const todos = [...this.state.todos]
+
+    todos.push(item)
+
+    this.setState({ todos })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>ToDo App</h1>
+        <TodoList todos={this.state.todos} addItem={this.addItem} />
+      </div>
+    )
+  }
 }
 
 export default App
